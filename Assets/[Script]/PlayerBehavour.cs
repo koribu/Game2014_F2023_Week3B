990a2f8df6b5386e238ec031f9667a5475330b5d
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-09-21 14:41:28 -0400

    Simple Touch input added for player behavior

--- Assets/[Script]/PlayerBehavour.cs
+++ Assets/[Script]/PlayerBehavour.cs
@@ -10,21 +10,34 @@ public class PlayerBehavour : MonoBehaviour
     [SerializeField]
     private Boundries _horizontalBoundries, _verticalBoundries;
 
+    Camera _camera;
+
         // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xAxis = Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
+ /*       float xAxis = Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
         float yAxis = Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime;
 
-        transform.position = new Vector3(transform.position.x + xAxis, transform.position.y + yAxis, 0); //new Vector3(xAxis, yAxis, 0);
+        transform.position = new Vector3(transform.position.x + xAxis, transform.position.y + yAxis, 0); //new Vector3(xAxis, yAxis, 0);*/
 
 
+        foreach(Touch touch in Input.touches)
+        {
+            Vector2 destination = _camera.ScreenToWorldPoint(touch.position);
+            transform.position = destination;
+            
+        }
+
+
+
+
+        //Boundries
         if(transform.position.x < _horizontalBoundries.min)
         {
             transform.position = new Vector3(_horizontalBoundries.max, transform.position.y, transform.position.z);
